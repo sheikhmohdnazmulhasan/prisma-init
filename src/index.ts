5f72cbf610cc3011af84ee6000e8a3e20e3b5a16
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-26 08:50:39 +0600

    get all post

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    const result = await prisma.post.create({
-        data: {
-            title: 'this is title',
-            content: 'this is content',
-            author: 'nafisa'
-        }
-    });
+    // const result = await prisma.post.create({
+    //     data: {
+    //         title: 'this is title',
+    //         content: 'this is content',
+    //         author: 'nafisa'
+    //     }
+    // });
+
+    const result = await prisma.post.findMany()
 
     console.log(result);
 }
