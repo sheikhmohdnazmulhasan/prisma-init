f660a6fa3863113ec6123f2efc685984fb1f1777
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-26 08:17:20 +0600

    migrate post

---
+++ prisma/migrations/20241025164558_post_table_created/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `author` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Made the column `content` on table `Post` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorId",
+ADD COLUMN     "author" TEXT NOT NULL,
+ALTER COLUMN "title" SET DATA TYPE TEXT,
+ALTER COLUMN "content" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "Profile";
+
+-- DropTable
+DROP TABLE "User";
