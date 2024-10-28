16364321bc8389c096c0970e06f9bf7b7284d074
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-28 19:45:01 +0600

    populate

--- src/new/reletionalQs.ts
+++ src/new/reletionalQs.ts
@@ -3,12 +3,22 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function relationalQs() {
-    // fluent api
+    //! fluent api
+    // const result = await prisma.user.findUnique({
+    //     where: {
+    //         id: 1
+    //     }
+    // }).post();
+
+    // ! populate
     const result = await prisma.user.findUnique({
         where: {
             id: 1
+        },
+        include: {
+            profile: true
         }
-    }).post();
+    })
 
 
     return result
